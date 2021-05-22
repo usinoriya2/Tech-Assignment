cfd68eebba8d484f0b356e19fa90fd98b80df3f1
utkarshsinoriya <utkarsh@tinkerix.com>
2021-05-23 00:17:01 +0530

    shimmer animation added

--- app/src/main/java/com/example/techassignment/MainActivity.java
+++ app/src/main/java/com/example/techassignment/MainActivity.java
@@ -4,11 +4,16 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.facebook.shimmer.ShimmerFrameLayout;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ShimmerFrameLayout shimmerFrameLayout =
+                (ShimmerFrameLayout) findViewById(R.id.shimmerFrameLayout);
+        shimmerFrameLayout.startShimmerAnimation();
     }
 }
