4751bc4fcf7361a6258548978cc7ae1cd1b5c36a
utkarshsinoriya <utkarsh@tinkerix.com>
2021-05-23 12:56:11 +0530

    API call added and setting repo cards dynamically

--- app/src/main/java/com/example/techassignment/MainActivity.java
+++ app/src/main/java/com/example/techassignment/MainActivity.java
@@ -1,66 +1,138 @@
 package com.example.techassignment;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
 
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.Volley;
+import com.example.techassignment.Models.Repository;
 import com.facebook.shimmer.ShimmerFrameLayout;
 
-public class MainActivity extends AppCompatActivity {
-    ShimmerFrameLayout shimmerFrameLayout;
-    LinearLayout descriptionLayout;
-    RelativeLayout repo1;
-    RelativeLayout repo2;
-    Handler handler;
-    Runnable r;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.logging.Logger;
+
+public class MainActivity extends AppCompatActivity {
+    private ShimmerFrameLayout shimmerFrameLayout;
+    private LinearLayout repoLayout;
+    private Handler handler;
+    private Runnable r;
+    private JsonArrayRequest mJsonArrayRequest;
+    private RequestQueue mRequestQueue;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         bindViews();
-        initialiseUI();
+        getRepositoryData();
     }
 
     private void bindViews(){
         shimmerFrameLayout = findViewById(R.id.shimmerFrameLayout);
-        repo1 = findViewById(R.id.repo1);
-        repo2 = findViewById(R.id.repo2);
-        descriptionLayout = findViewById(R.id.description_layout);
-        shimmerFrameLayout.startShimmerAnimation();
-    }
-    private void initialiseUI(){
+        repoLayout = findViewById(R.id.repo_layout);
         shimmerFrameLayout.setVisibility(View.VISIBLE);
         shimmerFrameLayout.startShimmerAnimation();
-        handler = new Handler();
-        r = new Runnable() {
-
-            @Override
-            public void run() {
-                shimmerFrameLayout.stopShimmerAnimation();
-                shimmerFrameLayout.setVisibility(View.GONE);
-                repo1.setVisibility(View.VISIBLE);
-                repo2.setVisibility(View.VISIBLE);
-                repo1.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        if(descriptionLayout.getVisibility() == View.GONE){
-                            descriptionLayout.setVisibility(View.VISIBLE);
-                        }else{
-                            descriptionLayout.setVisibility(View.GONE);
-                        }
-                    }
-                });
-            }
-        };
-        startHandler();
     }
 
-    public void startHandler() {
-        handler.postDelayed(r, 2000); //for 2 seconds
+
+    private void getRepositoryData(){
+        mJsonArrayRequest = new JsonArrayRequest(
+                "https://private-anon-cf6ffd2614-githubtrendingapi.apiary-mock.com/repositories", new Response.Listener<JSONArray>() {
+            @Override
+            public void onResponse(JSONArray response) {
+                List<Repository> repositoryList = new ArrayList<>();
+                for (int i = 0; i < response.length(); i++) {
+                    try {
+                        JSONObject jsonObject = response.getJSONObject(i);
+                        Repository repository = new Repository(jsonObject.getString("author"),jsonObject.getString("name"),jsonObject.getString("avatar"),
+                                jsonObject.getString("url"),jsonObject.getString("description"),jsonObject.getString("language"),jsonObject.getString("languageColor"),
+                                jsonObject.getInt("stars"),jsonObject.getInt("forks"),jsonObject.getInt("currentPeriodStars"));
+                        repositoryList.add(repository);
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                }
+                setRepositoryData(repositoryList);
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+        });
+
+        mRequestQueue = Volley.newRequestQueue(MainActivity.this);
+        mRequestQueue.add(mJsonArrayRequest);
+    }
+
+    private void setRepositoryData(List<Repository> repositoryList){
+        for(Repository repository: repositoryList){
+            makeRepositoryLayout(2000, repoLayout, repository);
+        }
+        shimmerFrameLayout.setVisibility(View.GONE);
+        shimmerFrameLayout.stopShimmerAnimation();
+    }
+
+    private void makeRepositoryLayout(int screenHeight,  LinearLayout view, Repository repository){
+        LayoutInflater inflater = (LayoutInflater) view.getContext().getSystemService(view.getContext().LAYOUT_INFLATER_SERVICE);
+        View repoView = inflater.inflate(R.layout.repository, null);
+        RelativeLayout.LayoutParams lp = new RelativeLayout.LayoutParams(
+                ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        lp.bottomMargin = 10;
+        repoView.setLayoutParams(lp);
+        TextView author = repoView.findViewById(R.id.author);
+        TextView name = repoView.findViewById(R.id.repo_name);
+        TextView description = repoView.findViewById(R.id.description);
+        ImageView avatar = repoView.findViewById(R.id.avatar);
+        final LinearLayout descriptionLayout = repoView.findViewById(R.id.description_layout);
+        CardView languageColor = repoView.findViewById(R.id.language_color);
+        TextView language = repoView.findViewById(R.id.language);
+        TextView stars = repoView.findViewById(R.id.stars);
+        TextView forks = repoView.findViewById(R.id.forks);
+
+        author.setText(repository.getAuthor());
+        name.setText(repository.getName());
+        description.setText(repository.getDescription());
+//        languageColor.setCardBackgroundColor(repository.getLanguageColor());
+        language.setText(repository.getLanguage());
+        stars.setText(String.valueOf(repository.getStars()));
+        forks.setText(String.valueOf(repository.getForks()));
+
+        Uri imgUri=Uri.parse(repository.getAvatar());
+        avatar.setImageURI(null);
+        avatar.setImageURI(imgUri);
+
+        repoView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(descriptionLayout.getVisibility() == View.GONE){
+                    descriptionLayout.setVisibility(View.VISIBLE);
+                }else{
+                    descriptionLayout.setVisibility(View.GONE);
+                }
+            }
+        });
+
+        view.addView(repoView);
     }
 }
---
+++ app/src/main/java/com/example/techassignment/Models/Repository.java
@@ -0,0 +1,110 @@
+package com.example.techassignment.Models;
+
+public class Repository {
+    private String author;
+    private String name;
+    private String avatar;
+    private String url;
+    private String description;
+    private String language;
+    private String languageColor;
+    private int stars;
+    private int forks;
+    private int currentPeriodStars;
+
+    public Repository() {
+    }
+
+    public Repository(String author, String name, String avatar, String url, String description, String language, String languageColor, int stars, int forks, int currentPeriodStars) {
+        this.author = author;
+        this.name = name;
+        this.avatar = avatar;
+        this.url = url;
+        this.description = description;
+        this.language = language;
+        this.languageColor = languageColor;
+        this.stars = stars;
+        this.forks = forks;
+        this.currentPeriodStars = currentPeriodStars;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAvatar() {
+        return avatar;
+    }
+
+    public void setAvatar(String avatar) {
+        this.avatar = avatar;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getLanguage() {
+        return language;
+    }
+
+    public void setLanguage(String language) {
+        this.language = language;
+    }
+
+    public String getLanguageColor() {
+        return languageColor;
+    }
+
+    public void setLanguageColor(String languageColor) {
+        this.languageColor = languageColor;
+    }
+
+    public int getStars() {
+        return stars;
+    }
+
+    public void setStars(int stars) {
+        this.stars = stars;
+    }
+
+    public int getForks() {
+        return forks;
+    }
+
+    public void setForks(int forks) {
+        this.forks = forks;
+    }
+
+    public int getCurrentPeriodStars() {
+        return currentPeriodStars;
+    }
+
+    public void setCurrentPeriodStars(int currentPeriodStars) {
+        this.currentPeriodStars = currentPeriodStars;
+    }
+}
